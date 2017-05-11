e8f9a5e773449421b7c6290706cde6053610fff1
Shuping <shuping.liu@centricconsulting.com>
2017-05-11 16:46:09 -0700

    Enable mocha as test framework (npm start) & last to-do app

---
+++ VSReact.Web/app/index.test.js
@@ -0,0 +1,13 @@
+﻿import expect from 'expect';
+
+describe('Our first test', () => {
+	it('should pass', () => {
+		expect(true).toEqual(true);
+	});
+});
+
+describe('Our 2nd test', () => {
+	it('should fail', () => {
+		expect(false).toEqual(true);
+	});
+});
--- VSReact.Web/webpack.config.js
+++ VSReact.Web/webpack.config.js
@@ -1,4 +1,4 @@
-/// <binding ProjectOpened='Hot' />
+/// <binding ProjectOpened='Hot, Watch - Development' />
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
